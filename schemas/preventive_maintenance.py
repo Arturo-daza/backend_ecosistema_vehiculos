e6d0081574ce0b8b6e8e62389d1f4443a599d876
tech <tech@gmail.com>
2024-10-13 15:49:01 -0500

    update schema and model of preventive maintenance

--- schemas/preventive_maintenance.py
+++ schemas/preventive_maintenance.py
@@ -5,17 +5,25 @@ from datetime import date
 class PreventiveMaintenanceBase(BaseModel):
     IdVehiculo: str
     IdServicio: int
-    KilometrajeRecomendado: Optional[int]
-    FrecuenciaTipo: str = 'Kilometraje'
-    FrecuenciaValor:int 
-    FechaUltimoMantenimiento: Optional[date]
+    FrecuenciaTipo: str = 'Kilometraje'  # Puede ser 'Kilometraje' o 'Tiempo'
+    FrecuenciaKilometraje: Optional[int]  # Frecuencia basada en kilometraje
+    FrecuenciaTiempo: Optional[int]  # Frecuencia basada en tiempo
+    FrecuenciaTiempoTipo: Optional[str] = 'Meses'  # Puede ser 'Dias', 'Semanas', 'Meses', 'Años'
+    FechaUltimoMantenimiento: Optional[date]  # Fecha del último mantenimiento
+    KilometrajeUltimoMantenimiento: Optional[int]  # Kilometraje al momento del último mantenimiento
     Notas: Optional[str]
+
     @field_validator('FrecuenciaTipo')
-    def validate_tipo_usuario(cls, value):
+    def validate_frecuencia_tipo(cls, value):
         if value not in ['Kilometraje', 'Tiempo']:
-            raise ValueError("Frecuencia tipo invalidad. Debe ser 'Tiempo' o 'Kilometraje'.")
+            raise ValueError("Frecuencia tipo inválida. Debe ser 'Tiempo' o 'Kilometraje'.")
         return value
-    
+
+    @field_validator('FrecuenciaTiempoTipo')
+    def validate_frecuencia_tiempo_tipo(cls, value):
+        if value not in ['Dias', 'Semanas', 'Meses', 'Años']:
+            raise ValueError("Frecuencia tiempo tipo inválida. Debe ser 'Dias', 'Semanas', 'Meses', o 'Años'.")
+        return value
 
     class Config:
         from_attributes = True
